e0bca7d3d6b45368973c6e6dc344cb25755f8bba
18morgand <18morgand@students.harker.org>
2017-05-11 15:06:31 -0700

    Update PhraseRep.java

--- PhraseRep.java
+++ PhraseRep.java
@@ -15,9 +15,6 @@ public class PhraseRep
     {
         phrases.add(new Phrase("et tu Brute"));
         phrases.add(new Phrase("My dear Watson"));
-        phrases.add(new Phrase("I don't think we're in Kansas anymore"));
-        phrases.add(new Phrase("You is kind. You is smart. You is important."));
-        phrases.add(new Phrase("Gentlemen, you can't fight in here! This is the war room!"));
     }
     public Phrase randomPhrase()
     {
