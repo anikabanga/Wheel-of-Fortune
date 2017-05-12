e3cac92568384608f6be9707dabd5ab1a4731b0a
18morgand <18morgand@students.harker.org>
2017-05-11 21:36:52 -0700

    Update WheelofFortune.java

--- WheelofFortune.java
+++ WheelofFortune.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import javax.swing.JOptionPane;
+import java.awt.Frame;
 /**
  * Write a description of class WheelofFortune here.
  * 
@@ -7,15 +9,28 @@ import java.util.*;
  */
 public class WheelofFortune
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    int[] possibilities = {2, 3};
+    private ArrayList<Player> players;
     /**
      * Constructor for objects of class WheelofFortune
      */
     public WheelofFortune()
     {
+        players = new ArrayList<Player>();
+        PhraseRep prep = new PhraseRep();
+        Phrase p = prep.randomPhrase();
+        JOptionPane pane = new JOptionPane();
+        Frame f = new Frame();
+        Object input = pane.showInputDialog(f, "Select number of players", "New game", JOptionPane.PLAIN_MESSAGE, 
+                                            null, possibilities, 2);
+        for(int i=0; i<((int)input); i++)
+        {
+            int z = i+1;
+            Object name = pane.showInputDialog(f,"Player"+ z + "'s name" , null, JOptionPane.PLAIN_MESSAGE);
+            players.add(new Player((String)name));
+        }
         
+    }
     }
 
 }
