53df658c7bee690c6d38a8e3fdb2d2c885740fa1
anikabanga <18anikab@students.harker.org>
2017-05-23 08:18:56 -0700

    Update PhraseRep.java

--- PhraseRep.java
+++ PhraseRep.java
@@ -15,6 +15,23 @@ public class PhraseRep
     {
         phrases.add(new Phrase("et tu Brute"));
         phrases.add(new Phrase("My dear Watson"));
+        phrases.add(new Phrase("Anika is Queen"));
+        phrases.add(new Phrase("Cup of Joe"));
+        phrases.add(new Phrase("My dear Watson"));
+        phrases.add(new Phrase("Feeling blue"));
+        phrases.add(new Phrase("Bye Felicia"));
+        phrases.add(new Phrase("Piece of Cake"));
+        phrases.add(new Phrase("Plead the Fifth"));
+        phrases.add(new Phrase("Spill the beans"));
+        phrases.add(new Phrase("Cut the cheese"));
+        phrases.add(new Phrase("Barack Obama"));
+        phrases.add(new Phrase("A Cheap Shot"));
+        phrases.add(new Phrase("Choose Happiness"));
+        phrases.add(new Phrase("All is Well"));
+        phrases.add(new Phrase("Time will Tell"));
+        phrases.add(new Phrase("Life is Short"));
+        phrases.add(new Phrase("Rise up"));
+        phrases.add(new Phrase("Youll be Back"));
     }
     public Phrase randomPhrase()
     {
