53bd7f9c5f464e6e326f5be1e90b0b289bc40fb3
18juliah <18juliah@students.harker.org>
2017-05-23 08:19:56 -0700

    Update Letter.java

--- Letter.java
+++ Letter.java
@@ -8,26 +8,14 @@
 public class Letter
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String str;
 
     /**
      * Constructor for objects of class Letter
      */
-    public Letter()
+    public Letter(String a)
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        str = a;
     }
 }
