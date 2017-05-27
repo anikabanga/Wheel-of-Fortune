a3f6ca0373c5f2dc90daedc4a0ec0f398e4b5f27
18morgand <18morgand@students.harker.org>
2017-05-27 00:14:13 -0700

    Update Letter.java

--- Letter.java
+++ Letter.java
@@ -8,14 +8,41 @@
 public class Letter
 {
     // instance variables - replace the example below with your own
-    private String str;
-
+    private String hidden;
+    private String revealed;
+    private boolean state;
+    private String let;
     /**
      * Constructor for objects of class Letter
      */
-    public Letter(String a)
+    public Letter(String letter)
     {
-        // initialise instance variables
-        str = a;
+        let = letter.toLowerCase();
+        hidden = new String("hidden.gif");
+        revealed = new String(letter.toUpperCase() + ".gif");
+        state = false;
+    }
+
+    public String getImageFileName()
+    {
+        if(state)
+        {
+            return revealed;
+        }
+        else
+        {
+            if(let.equals(" "))
+                return "default";
+            else
+                return hidden;
+        }
+    }
+    public String value()
+    {
+        return let;
+    }
+    public void reveal()
+    {
+        state = true;
     }
 }
