b9ac38c96a47a90be603651b8a73cc01793a91c9
18morgand <18morgand@students.harker.org>
2017-05-27 00:14:50 -0700

    Update Phrase.java

--- Phrase.java
+++ Phrase.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class Phrase here.
  * 
@@ -8,17 +8,27 @@
 public class Phrase
 {
     // instance variables - replace the example below with your own
+    private ArrayList<Letter> letters;
     private String str;
-
     /**
      * Constructor for objects of class Phrase
      */
     public Phrase(String s)
     {
         str = s;
+        letters = new ArrayList<Letter>();
+        for(int i=0; i<s.length(); i++)
+        {
+            letters.add(new Letter(s.substring(i, i+1)));
+        }
     }
+
     public String toString()
     {
-        return str;
+        return str.toLowerCase();
+    }
+    public ArrayList<Letter> getLetters()
+    {
+        return letters;
     }
 }
