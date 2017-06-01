0ebbf4efe0d2844532f67ac8a6d3e44bb341c8ef
anikabanga <18anikab@students.harker.org>
2017-06-01 08:23:38 -0700

    Update MyBoundedGrid.java

--- MyBoundedGrid.java
+++ MyBoundedGrid.java
@@ -1,108 +1,118 @@
-import java.util.ArrayList;
-/**
- * A MyBoundedGrid is a rectangular grid with a finite number of rows and columns.
- * @author Morgan Douglas
- * @version 02/23/17
- * @param <E> type of object being put into grid
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2002-2006 College Entrance Examination Board 
+ * (http://www.collegeboard.com).
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Alyce Brady
+ * @author APCS Development Committee
+ * @author Cay Horstmann
  */
-public class MyBoundedGrid<E>
+
+ 
+
+import java.util.ArrayList;
+
+/**
+ * A <code>BoundedGrid</code> is a rectangular grid with a finite number of
+ * rows and columns. <br />
+ * The implementation of this class is testable on the AP CS AB exam.
+ */
+public class BoundedGrid<E> extends AbstractGrid<E>
 {
-    private Object[][] occupantArray; 
+    private Object[][] occupantArray; // the array storing the grid elements
 
     /**
-     * Constructor
-     * @precondition rows > 0 and cols > 0
-     * @param rows the number of rows in grid
-     * @param cols the number of columns in grid
+     * Constructs an empty bounded grid with the given dimensions.
+     * (Precondition: <code>rows > 0</code> and <code>cols > 0</code>.)
+     * @param rows number of rows in BoundedGrid
+     * @param cols number of columns in BoundedGrid
      */
-    public MyBoundedGrid(int rows, int cols)
+    public BoundedGrid(int rows, int cols)
     {
+        if (rows <= 0)
+            throw new IllegalArgumentException("rows <= 0");
+        if (cols <= 0)
+            throw new IllegalArgumentException("cols <= 0");
         occupantArray = new Object[rows][cols];
     }
 
-    /**
-     * @return number of rows
-     */
     public int getNumRows()
     {
         return occupantArray.length;
     }
 
-    /**
-     * @return number of columns
-     */
     public int getNumCols()
     {
+        // Note: according to the constructor precondition, numRows() > 0, so
+        // theGrid[0] is non-null.
         return occupantArray[0].length;
-
     }
 
-    /**
-     * @precondition: loc is not null
-     * @param loc the location being evaluated
-     * @return true if location is valid on grid; false otherwise
-     */
     public boolean isValid(Location loc)
     {
-        return (loc.getRow()<getNumRows() && loc.getCol() < getNumCols()) && 
-        (loc.getRow()>=0 && loc.getCol()>=0);
-
+        return 0 <= loc.getRow() && loc.getRow() < getNumRows()
+                && 0 <= loc.getCol() && loc.getCol() < getNumCols();
     }
 
-    /**
-     * @precondition loc is valid in grid
-     * @param loc the location being looked at
-     * @return object at loc
-     */
-    public E get(Location loc)
-    {
-
-        return (E) occupantArray[loc.getRow()][loc.getCol()];
-
-        //(You will need to promise the return value is of type E.)
-    }
-
-    /**
-     * @precondition loc is valid in grid
-     * @param obj the object being placed at loc
-     * @param loc the location obj is being placed
-     * @return item previously at loc; null if no item there
-     */
-    public E put(Location loc, E obj)
-    {
-        E save = get(loc);
-        occupantArray[loc.getRow()][loc.getCol()] = obj;
-        return save;
-    }
-
-    /**
-     * removes object at given location
-     * @param loc the location from which object is being removed
-     * @return object previously contained at loc
-     */
-    public E remove(Location loc)
-    {
-        E save = get(loc);
-        occupantArray[loc.getRow()][loc.getCol()] = null;
-        return save;
-    }
-
-    /**
-     * @return an ArrayList containing all occupied locations in grid
-     */
     public ArrayList<Location> getOccupiedLocations()
     {
-        ArrayList<Location> locs = new ArrayList<Location>();
-        for(int i=0; i<getNumRows(); i++)
+        ArrayList<Location> theLocations = new ArrayList<Location>();
+
+        // Look at all grid locations.
+        for (int r = 0; r < getNumRows(); r++)
         {
-            for( int z=0; z<getNumCols(); z++)
+            for (int c = 0; c < getNumCols(); c++)
             {
-                if(occupantArray[i][z]!=null)
-                {
-                    locs.add(new Location(i, z));
-                }
+                // If there's an object at this location, put it in the array.
+                Location loc = new Location(r, c);
+                if (get(loc) != null)
+                    theLocations.add(loc);
             }
         }
-        return locs;
+
+        return theLocations;
+    }
+
+    public E get(Location loc)
+    {
+        if (!isValid(loc))
+            throw new IllegalArgumentException("Location " + loc
+                    + " is not valid");
+        return (E) occupantArray[loc.getRow()][loc.getCol()]; // unavoidable warning
+    }
+
+    public E put(Location loc, E obj)
+    {
+        if (!isValid(loc))
+            throw new IllegalArgumentException("Location " + loc
+                    + " is not valid");
+        if (obj == null)
+            throw new NullPointerException("obj == null");
+
+        // Add the object to the grid.
+        E oldOccupant = get(loc);
+        occupantArray[loc.getRow()][loc.getCol()] = obj;
+        return oldOccupant;
+    }
+
+    public E remove(Location loc)
+    {
+        if (!isValid(loc))
+            throw new IllegalArgumentException("Location " + loc
+                    + " is not valid");
+        
+        // Remove the object from the grid.
+        E r = get(loc);
+        occupantArray[loc.getRow()][loc.getCol()] = null;
+        return r;
     }
 }
