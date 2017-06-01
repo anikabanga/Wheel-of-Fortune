2ab48149750ea5913249244b507db2d51af6a2bf
anikabanga <18anikab@students.harker.org>
2017-06-01 08:29:55 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,34 +1,41 @@
 
 /**
- * Write a description of class Player here.
+ * Player for Wheel of Fortune game
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Morgan Douglas, Anika Banga, Julia Huang
+ * @version June 1, 2017
  */
 public class Player
 {
+    //instance variables
     private String name;
     private int balance;
+    
     /**
      * Constructor for objects of class Player
+     *@param    n   name
      */
     public Player(String n)
     {
         name = n;
         balance = 0;
     }
+    //@return   the balance
     public int getAmt()
     {
         return balance;
     }
+    //@return   the name of the player
     public String getName()
     {
         return name;
     }
+    //@param    the new balance
     public void setAmt(int x)
     {
         balance = x;
     }
+    //@param the amount of money the player is making
     public void pay(int x)
     {
         balance+=x;
